c1b6d8f530a28b0dd60a2a06a722aeab146160db
keitasasaki4 <jcpmb136595@yahoo.co.jp>
2020-06-16 13:00:19 +0000

    add column and validation

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -9,6 +9,11 @@
       {!! Form::model($task,['route'=>'tasks.store'])!!}
       
         <dic class="form-group">
+          {!! Form::label('status','ステータス')!!}
+          {!! Form::text('status',null,['class'=>'form-control'])!!}
+        </dic>
+      
+        <dic class="form-group">
           {!! Form::label('content','タスク')!!}
           {!! Form::text('content',null,['class'=>'form-control'])!!}
         </dic>
